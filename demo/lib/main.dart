adc01fb503acf5c47fdec50a70d8e4dd3dc82b0f
Pradnya2000 <43577322+Pradnya2000@users.noreply.github.com>
2020-05-18 23:13:42 +0530

    stateful widget demo

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,66 +1,53 @@
-
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'app_screens/first_screen.dart';
 
 void main()
 {
   runApp(MaterialApp(
-    title: "Exploring UI",
-    home:Scaffold(
-      appBar: AppBar(
-        title:Text("List Items")
-      ),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          debugPrint("floatingButton Pressed");
-        },
-        child: Icon(Icons.add),
-        tooltip: "Add item",  
-      ),
-  
-    )
+    title: "StateFul App",
+    home:favCity()
   )
 );
 }
-
-List<String> getListElements()
+class favCity extends StatefulWidget
 {
-  var items=List<String>.generate(1000, (counter) => "Item $counter");
-  return items;
-} 
-
-void showSnackbar(BuildContext context,String msg)
-{
-  var snackbar=SnackBar(
-    content: Text("You tapped on $msg"),
-    action:SnackBarAction(
-      label: "UNDO",
-      onPressed:()
-      {
-        debugPrint("performing Dummy Undo");
-      })
-  );
-  Scaffold.of(context).showSnackBar(snackbar);
+  @override
+  State<StatefulWidget> createState() {
+    return _favCityState();
+  }
 }
-Widget getListView()
+
+class _favCityState extends State<favCity>
 {
-  var listdata=getListElements();
-  var listView=ListView.builder(
-    itemBuilder: (context ,index)
-    {
-      return ListTile(
-            leading: Icon(Icons.arrow_forward_ios),
-            title:Text(listdata[index]),
-            onTap: ()
-            {
-              showSnackbar(context,listdata[index]);
-            },
-      );
-    }
-  );
-  return listView;
+  String nameOfCity="";
+  @override
+  Widget build(BuildContext context) {
+   
+   return Scaffold(
+     appBar:AppBar(
+       title:Text("Stateful widget")
+     ),
+     body:Container(
+       margin: EdgeInsets.all(10.0),
+       padding: EdgeInsets.all(10.0),
+       child:Column(children: <Widget>[
+          TextField(
+            onSubmitted:(String userInput){
+              setState(() {
+                 nameOfCity=userInput;
+              });
+               
+            }
+          ),
+          Padding(padding: EdgeInsets.all(10.0),
+          child: Text("Your Favourite city is $nameOfCity")
+          )
+         
+
+       ],)
+     )
+    
+
+   );
+  }
 
 }
