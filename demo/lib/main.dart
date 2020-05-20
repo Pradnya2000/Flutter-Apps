7509e91110fe461a9da4368a516aef29d3235c28
Pradnya2000 <43577322+Pradnya2000@users.noreply.github.com>
2020-05-20 11:29:56 +0530

    Info Card

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,71 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(MaterialApp(
-    title: "StateFul App",
-    home:favCity()
-  )
-);
-}
-class favCity extends StatefulWidget
-{
-  @override
-  State<StatefulWidget> createState() {
-    return _favCityState();
-  }
-}
-
-class _favCityState extends State<favCity>
-{
-  String nameOfCity="";
-  var _currencies=['rupees','doller','pound','other'];
-  var _currentvalueselected='rupees';
-  @override
-  Widget build(BuildContext context) {
-   
-   return Scaffold(
-     appBar:AppBar(
-       title:Text("Stateful widget")
-     ),
-     body:Container(
-       margin: EdgeInsets.all(10.0),
-       padding: EdgeInsets.all(10.0),
-       child:Column(children: <Widget>[
-          TextField(
-            onSubmitted:(String userInput){
-              setState(() {
-                 nameOfCity=userInput;
-              });
-               
-            }
+//main fuction is starting point of every flutter app
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar:AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.blueGrey,
           ),
-          DropdownButton<String>(
-              items: _currencies.map((String dropDownStringItem)
-              {
-                return DropdownMenuItem(
-                  value: dropDownStringItem,
-                  child:Text(dropDownStringItem)
-                  );
-              }).toList(),
-              onChanged: (String newValueSelected)
-              {
-                setState(() {
-                  _currentvalueselected=newValueSelected;
-                });
-              },
-              value: _currentvalueselected,
+          backgroundColor: Colors.blueGrey[900],
+          body: Center(
+            child: Image(
+              image:AssetImage('images/diamond.png'), 
+            ),
           ),
-          Padding(padding: EdgeInsets.all(10.0),
-          child: Text("Your Favourite city is $nameOfCity")
-          )
-         
-
-       ],)
-     )
-    
-
-   );
-  }
-
-}
+        ),
+      ),
+    );
